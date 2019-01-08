d2cd6320f85c8b2d172864a2de57966c81e45a07
wumuwumu <2232728687@qq.com>
2019-01-08 16:55:28 +0800

    feat:添加eureka

---
+++ stop-eureka/src/main/java/cn/sciento/shop/eureka/EurekaApplication.java
@@ -0,0 +1,14 @@
+package cn.sciento.shop.eureka;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] arg){
+        SpringApplication.run(EurekaApplication.class,arg);
+    }
+}
